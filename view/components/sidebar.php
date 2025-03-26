03f04526202db768ddfc26bf507128445453fc89
Vitor Novaga <vitor7633136@aluno.ms.senac.br>
2025-03-26 19:22:51 -0400

    salvando

--- view/components/sidebar.php
+++ view/components/sidebar.php
@@ -1,19 +1,29 @@
-<aside>
-    Sidebar
-    <button type="button">OK</button>
+<?php
+// Exemplo de inclusão segura de arquivos usando __DIR__
+$menuItems = [
+    ['link' => __DIR__ . '/../view/pages/index.php', 'label' => 'Home', 'icon' => 'fas fa-home'],
+    ['link' => __DIR__ . '/../view/pages/categorias.php', 'label' => 'Categorias', 'icon' => 'fas fa-list'],
+    ['link' => __DIR__ . '/../view/pages/produtos.php', 'label' => 'Produtos', 'icon' => 'fas fa-box']
+];
 
+// Incluindo os arquivos necessários (opcional, caso sejam script ativos em PHP)
+require_once(__DIR__ . '/../view/pages/index.php');
+require_once(__DIR__ . '/../view/pages/categorias.php');
+require_once(__DIR__ . '/../view/pages/produtos.php');
+?>
+
+<aside>
+    <button type="button" aria-label="Menu de navegação">Menu</button>
     <nav>
         <ul>
-            <li>
-                <a href="/../site-adm-grid/view/pages/index.php">Home</a>
-            </li>
-            <li>
-                <a href="/../site-adm-grid/view/pages/categorias.php">Categorias</a>
-            </li>
-            <li>
-                <a href="/../site-adm-grid/view/pages/produtos.php">Produtos</a>
-            </li>
+            <?php foreach ($menuItems as $item): ?>
+                <li>
+                    <a href="<?php echo htmlspecialchars($item['link']); ?>">
+                        <i class="<?php echo htmlspecialchars($item['icon']); ?>"></i>
+                        <?php echo htmlspecialchars($item['label']); ?>
+                    </a>
+                </li>
+            <?php endforeach; ?>
         </ul>
     </nav>
-
 </aside>
--- view/pages/index.php
+++ view/pages/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
-    <link rel="stylesheet" href="/../site-adm-grid/view/assets/css/style.css">
+    <link rel="stylesheet" href="/../Vitor/siteadm/view/assets/css/style.css">
 </head>
 
 <body>
